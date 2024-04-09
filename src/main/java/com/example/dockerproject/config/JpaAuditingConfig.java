18f7263b22acb61f7f2ef948e53768d77177285d
kyungho1224 <dev.kyungho.kim@gmail.com>
2024-04-09 15:42:59 +0900

    JpaAuditing 활성화 설정 추가

---
+++ src/main/java/com/example/dockerproject/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.example.dockerproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
