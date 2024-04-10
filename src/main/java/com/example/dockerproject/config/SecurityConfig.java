388006b578bc7341099faae6ab547662fafd868c
kyungho1224 <dev.kyungho.kim@gmail.com>
2024-04-10 15:03:45 +0900

    EC2 HTTPS 적용, 시큐리티 cors 적용

--- src/main/java/com/example/dockerproject/config/SecurityConfig.java
+++ src/main/java/com/example/dockerproject/config/SecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -26,17 +31,31 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowedOriginPatterns(List.of(
+              "https://kimkyungho.p-e.kr",
+              "https://52.78.187.122"
+            ));
+            config.setAllowCredentials(true);
+            return config;
+        };
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
           .httpBasic(AbstractHttpConfigurer::disable)
+          .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
           .csrf(AbstractHttpConfigurer::disable)
           .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
           .authorizeHttpRequests(request -> request
             .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
             .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
             .requestMatchers("/open-api/**").permitAll()
-//            .requestMatchers("/api/**").permitAll()
             .anyRequest().authenticated()
           )
           .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
